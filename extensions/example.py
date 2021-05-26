2e5dee1f42d82bd087463a222b271df3eafc5fc4
XenocidePC <xenocidepc@gmail.com>
2021-05-26 20:39:51 +1000

    Add example cog in extensions folder

---
+++ extensions/example.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class Example(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def examplecommand(self, ctx):
+        pass
+
+
+def setup(bot):
+    bot.add_cog(Example(bot))
