1db609972046981ced24fb0cca30a7436d015d75
Rifa Jamal <rifajamal@192-168-1-21.tpgi.com.au>
2021-06-02 20:45:27 +1000

    separated message and command logging; also put into a Cog/class

--- log.py
+++ log.py
@@ -3,133 +3,67 @@ import discord
 from discord.ext import commands
 import logging
 import traceback
-
-import os
 from discord.ext.commands.errors import BadArgument
-import yaml
-from dotenv import load_dotenv
 
-# Load settings file and set variables
-with open('./config/settings.yml') as file:
-    settings = yaml.full_load(file)
-
-BOT_PREFIX = settings['prefix']
-
-# Set token
-if settings['token_env_enabled']:
-    load_dotenv()
-    BOT_TOKEN = os.getenv(settings['token_env_key'])
-else:
-    BOT_TOKEN = settings['token']
-
-# TODO: Move this to config file
-intents = discord.Intents.default()
-
-# Initialise bot
-bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents)
-
-# Load extensions
-if __name__ == "__main__":
-    for extension in settings['enabled_extensions']:
-        bot.load_extension(f"extensions.{extension}")
 
 ##############################################################
 #               LOGGING (General + Error Handling)           #
 ##############################################################
 
-# General logging
-@bot.event
-async def on_command(ctx):
-    # Log format (Message contains user, server and command typed in)
-    logging.basicConfig(filename='general.log', \
-        filemode = 'a', \
-        format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
-        encoding='utf-8', \
-        level=logging.INFO)
-    server = ctx.guild.name
-    user = ctx.author
-    message = ctx.message.content
-    logging.info(f'{server} - {user} - {message}')
+class CommandLogging(commands.Cog):
 
-#  Bot error messages
-@bot.event
-async def on_error(ctx):
-    # Error Log format    
-    logging.basicConfig(filename='error.log', \
-    filemode = 'a', format='%(asctime)s - %(message)s', \
-    datefmt='%d-%b-%y %H:%M:%S', \
-    encoding='utf-8', \
-    level=logging.ERROR)
-    server = ctx.guild.name
-    user = ctx.author
-    message = ctx.message.content
-    # Log error
-    logging.error(f'{server} - {user} - {traceback.format_exc()}')
+    def __init__(self, bot):
+        self.bot = bot
 
-# User Command error messages
-@bot.event
-async def on_command_error(ctx, error):
-    # Error Log Format
-    logging.basicConfig(filename='command_error.log', \
-        filemode = 'a', \
-        format='%(asctime)s - %(message)s', \
+    @commands.Cog.listener()
+    # General logging
+    async def on_command(self, ctx):
+        # Log format (Message contains user, server and command typed in)
+        logging.basicConfig(filename='general.log', \
+            filemode = 'a', \
+            format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
+            encoding='utf-8', \
+            level=logging.INFO)
+        server = ctx.guild.name
+        user = ctx.author
+        message = ctx.message.content
+        logging.info(f'{server} - {user} - {message}')
+
+    @commands.Cog.listener()
+    #  Bot error messages
+    async def on_error(self, ctx):
+        # Error Log format    
+        logging.basicConfig(filename='error.log', \
+        filemode = 'a', format='%(asctime)s - %(message)s', \
         datefmt='%d-%b-%y %H:%M:%S', \
-        encoding='utf-8', level=logging.ERROR)
-    server = ctx.guild.name
-    user = ctx.author
-    message = ctx.message.content
-
-    if ((isinstance(error, commands.MissingRequiredArgument)) or (isinstance(error, commands.BadArgument))):
-        logging.error(f'{server} - {user} - {message} - Invalid Arguments')
-    
-    elif (isinstance(error, commands.CommandNotFound)):
-        logging.error(f'{server} - {user} - {message} - Command not found.')
-
-
-
-###############################################################
-#                      MESSAGE LOGGING                        #
-###############################################################
-
-# Messages
-
-@bot.event
-async def on_message(message):
-     logging.basicConfig(filename='message.log', \
-        filemode = 'a', \
-        format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
         encoding='utf-8', \
-        level=logging.INFO)
-    server = ctx.guild.name
-    user = ctx.author
-    message = ctx.message.content
-    
-    logging.info(f'{server} - {user} - {message}')
+        level=logging.ERROR)
+        server = ctx.guild.name
+        user = ctx.author
+        message = ctx.message.content
+        # Log error
+        logging.error(f'{server} - {user} - {traceback.format_exc()}')
 
-@bot.event
-async def on_message_edit(message):
-    logging.basicConfig(filename='message.edit.log', \
-        filemode = 'a', \
-        format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
-        encoding='utf-8', \
-        level=logging.INFO)
-    server = ctx.guild.name
-    user = ctx.author
-    message_before = message_before.content
-    message_after = message_after.content
+    @commands.Cog.listener()
+    # User Command error messages
+    async def on_command_error(self, ctx, error):
+        # Error Log Format
+        logging.basicConfig(filename='command_error.log', \
+            filemode = 'a', \
+            format='%(asctime)s - %(message)s', \
+            datefmt='%d-%b-%y %H:%M:%S', \
+            encoding='utf-8', level=logging.ERROR)
+        server = ctx.guild.name
+        user = ctx.author
+        message = ctx.message.content
 
-    logging.info(f'{user} edited message in {server} \n Message before:{message_before} \n Message after:{message_after}')
+        if ((isinstance(error, commands.MissingRequiredArgument)) or (isinstance(error, commands.BadArgument))):
+            logging.error(f'{server} - {user} - {message} - Invalid Arguments')
+        
+        elif (isinstance(error, commands.CommandNotFound)):
+            logging.error(f'{server} - {user} - {message} - Command not found.')
 
 
-@bot.event
-async def on_message_delete(message):
-    logging.basicConfig(filename='message.delete.log', \
-        filemode = 'a', \
-        format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
-        encoding='utf-8', \
-        level=logging.INFO)
-    server = ctx.guild.name
-    user = ctx.author
-    message = ctx.message.content
-   
-    logging.info (f'{server} - {user} - {message} - deleted')
+
+def setup(bot):
+    bot.add_cog(CommandLogging(bot))
---
+++ message_log.py
@@ -0,0 +1,61 @@
+from gc import is_finalized
+import discord
+from discord.ext import commands
+from discord.ext import Cog
+import logging
+import traceback
+from discord.ext.commands.errors import BadArgument
+
+
+###############################################################
+#                      MESSAGE LOGGING                        #
+###############################################################
+class MessageLogging(Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @Cog.listener()
+    async def on_message(self, ctx, message):
+        logging.basicConfig(filename='message.log', \
+            filemode = 'a', \
+            format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
+            encoding='utf-8', \
+            level=logging.INFO)
+        server = ctx.guild.name
+        user = ctx.author
+        message = ctx.message.content
+        
+        logging.info(f'{server} - {user} - {message}')
+
+    @Cog.listener()
+    async def on_message_edit(self, ctx, before, after, message):
+        logging.basicConfig(filename='message.edit.log', \
+            filemode = 'a', \
+            format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
+            encoding='utf-8', \
+            level=logging.INFO)
+        server = ctx.guild.name
+        user = ctx.author
+        message_before = before.content
+        message_after = after.content
+
+        logging.info(f'{user} edited message in {server} \n Message before:{message_before} \n Message after:{message_after}')
+
+
+    @Cog.listener()
+    async def on_message_delete(self, ctx, message):
+        logging.basicConfig(filename='message.delete.log', \
+            filemode = 'a', \
+            format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', \
+            encoding='utf-8', \
+            level=logging.INFO)
+        server = ctx.guild.name
+        user = ctx.author
+        message = ctx.message.content
+    
+        logging.info (f'{server} - {user} - {message} - deleted')
+
+
+def setup(bot):
+    bot.add_cog(MessageLogging(bot))
