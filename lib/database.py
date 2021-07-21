69bee90f99cc4299f24895c3a0afb5f43310e3bc
Rifa Jamal <rifajamal@192-168-1-12.tpgi.com.au>
2021-07-21 17:39:39 +1000

    added user create tables to database

--- lib/database.py
+++ lib/database.py
@@ -1,10 +1,65 @@
 import psycopg2
 
-con = psycopg2.connect(user="user", password="pass", host="localhost", port="5432")
+def create_user_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE users (
+            userid INTEGER PRIMARY KEY,
+            joindate DATE NOT NULL,
+            leavedate DATE,
+            left boolean
+        )
+        """,
+        """
+        CREATE TABLE user_roles (
+            rid INTEGER PRIMARY KEY,
+            userid INTEGER NOT NULL,
+            role varchar(64),
+            FOREIGN KEY (userid),
+            REFERENCES users (userid)
+        )
+        """,
 
-cur = con.cursor()
-cur.execute('')
-# Note create table
+        """
+        CREATE TABLE user_channels (
+            cid INTEGER PRIMARY KEY,
+            userid INTEGER NOT NULL,
+            channel varchar(64),
+            FOREIGN KEY (userid),
+            REFERENCES users (userid)
+        )
+        """,
 
-con.commit()
-con.close()
+        """
+        CREATE TABLE user_permission (
+            pid INTEGER PRIMARY KEY,
+            userid INTEGER NOT NULL,
+            permission varchar(64)
+            FOREIGN KEY (userid)
+            REFERENCES users (userid)
+        )
+        """
+    )
+
+    con = None
+    try:
+        # connect to the PostgreSQL server
+        con = psycopg2.connect(user="user", password="pass", host="localhost", port="5432")
+        cur = con.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        con.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if con is not None:
+            con.close()
+
+
+if __name__ == '__main__':
+    create_user_tables()
