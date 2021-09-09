e97bb4ea3bf39b399dd6c0f9663ce4928220cec6
XenocidePC <xenocidepc@gmail.com>
2021-09-09 22:42:49 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const { Client, Collection, Intents } = require("discord.js");
+require("dotenv").config();
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Add commands to the client
+client.commands = new Collection();
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// Add events to the client
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+// Handle commands
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
