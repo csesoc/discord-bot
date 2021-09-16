2da6ce0f96878ac489bc052888573a5f010fce86
JeremyBearimy90 <68788055+JeremyBearimy90@users.noreply.github.com>
2021-09-16 15:24:21 +1000

    Added usage message on error

--- extensions/schedule_message.py
+++ extensions/schedule_message.py
@@ -11,21 +11,31 @@ class schedule_message(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator=True)
     async def schedule_send(self, ctx, date, time, channel_id):
-        if ctx.message.reference:
-            message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-            print(message.content)
-
+        try:
             scheduled_time = date + " " + time        
-        
+            dt_time = datetime.strptime(scheduled_time, "%Y/%m/%d %H:%M")
+            channel_id = channel_id[2:-1]
+            channel = self.bot.get_channel(int(channel_id))
+            if not channel:
+                raise ValueError
+            if not ctx.message.reference:
+                raise ValueError
+
+            message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+            message_content = message.content
+
             with open("data/scheduled_messages.json", 'r') as f:
                 schedule_list = json.load(f)
-            
-            schedule_list.append([scheduled_time, channel_id[2:-1], message.content])
+
+            schedule_list.append([scheduled_time, channel_id, message_content])
 
             with open("data/scheduled_messages.json", 'w') as f:
                 schedule_list = json.dump(schedule_list, f)
 
             await ctx.message.add_reaction("👍")
+
+        except ValueError:
+            await ctx.send("USAGE:\n\n$schedule_send YYYY/MM/DD HH:MM #target-channel\nThis message must be a REPLY to the reminder message you want to send", delete_after=10)
 
 
     @tasks.loop(seconds=60)
@@ -38,7 +48,6 @@ class schedule_message(commands.Cog):
                 if send_time == time_now:
                     channel = self.bot.get_channel(int(todo[1]))
                     await channel.send(todo[2])
-                    print("message sent!")
 
 def setup(bot):
     bot.add_cog(schedule_message(bot))
