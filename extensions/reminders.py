820190a1df66298d181df9f3f8df4c7ff7fd677e
JeremyBearimy90 <68788055+JeremyBearimy90@users.noreply.github.com>
2021-09-16 16:28:04 +1000

    Added option to set reminder directly by DMing the bot

--- extensions/reminders.py
+++ extensions/reminders.py
@@ -12,44 +12,57 @@ class reminders(commands.Cog):
     async def schedule_reminder(self, ctx, date, time, *contents):
         try:
             scheduled_time = datetime.strptime(date + " " + time, "%Y/%m/%d %H:%M")
-            if contents:
-                print(date, time, ctx.message.id)
-
-                with open("data/reminders.json", 'r') as f:
-                    reminders_list = json.load(f)
-
-                reminders_list.append([date + " " + time, ctx.channel.id, ctx.message.id])
-
-                with open("data/reminders.json", 'w') as f:
-                    json.dump(reminders_list, f)
-            else:
+            if not contents:
                 raise ValueError
+
         except ValueError:
             await ctx.send("USAGE for schedule_reminder:\n\n$schedule_reminder YYYY/MM/DD HH:MM\nfollowed by the message contents on a new line.", delete_after=5)
+
+        with open("data/reminders.json", 'r') as f:
+            reminders_list = json.load(f)
+
+        reminders_list.append([date + " " + time, ctx.channel.id, ctx.message.id])
+
+        with open("data/reminders.json", 'w') as f:
+            json.dump(reminders_list, f)
+
+        if isinstance(ctx.channel, discord.channel.DMChannel):
+            await ctx.send("Reminder has been saved!")
+        else:
+            await ctx.send("React to the message above to receive the reminder as a private message.")
 
 
     @tasks.loop(seconds=60)
     async def send_messages(self):
         time_now = datetime.now().replace(second=0, microsecond=0)
+
         with open("data/reminders.json", 'r') as f:
             to_send_list = json.load(f)
+
         for to_send in to_send_list:
             send_time = datetime.strptime(to_send[0], "%Y/%m/%d %H:%M")
+
             if send_time == time_now:
                 original_channel = self.bot.get_channel(to_send[1])
                 message = await original_channel.fetch_message(to_send[2])
 
-                users = set()
-                for reaction in message.reactions:
-                    async for user in reaction.users():
-                        users.add(user)
+                if isinstance(original_channel, discord.channel.DMChannel):
+                    content = message.content.split("\n")
+                    content = "\n".join(content[1:])
+                    e = discord.Embed(title="Reminder!", description=content, color=0xFF0000)
+                    await original_channel.send(embed=e)
 
-                content = message.content.split("\n")
-                content = "\n".join(content[1:])
-                e = discord.Embed(title="Reminder!", description=content, color=0xFF0000)
-                for user in users:
-                    await user.send(embed=e)
-                    print("Done")
+                else:
+                    users = set()
+                    for reaction in message.reactions:
+                        async for user in reaction.users():
+                            users.add(user)
+
+                    content = message.content.split("\n")
+                    content = "\n".join(content[1:])
+                    e = discord.Embed(title="Reminder!", description=content, color=0xFF0000)
+                    for user in users:
+                        await user.send(embed=e)
 
 def setup(bot):
     bot.add_cog(reminders(bot))
