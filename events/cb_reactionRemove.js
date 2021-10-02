0b9e18db7253a8efb78ac0a6dcb4e849f9c36d21
ollibowers <80164276+ollibowers@users.noreply.github.com>
2021-10-02 19:43:45 +1000

    setup the events, and base classes

--- events/cb_reactionRemove.js
+++ events/cb_reactionRemove.js
@@ -1,7 +1,31 @@
+//@ts-check
+
+const { MessageReaction, User } = require("discord.js");
+const { CarrotboardStorage } = require("../lib/carrotboard");
+
 module.exports = {
     name: "messageReactionRemove",
     once: false,
-    execute(messageReaction, user) {
-        console.log("reaction remove to msg");
+    /**
+     * @param {MessageReaction} reaction
+     * @param {User} user
+     */
+    async execute(reaction, user) {
+        /** @type {CarrotboardStorage} */
+        const cbStorage = global.cbStorage;
+        const message = reaction.message;
+        
+        if (!message.author.bot && !reaction.me) {
+            // get the details
+            const emoji = reaction.emoji.name;
+            const messageID = message.id;
+            const channelID = message.channelId;
+            const authorID = message.author.id;
+
+            // subtract from storage
+            await cbStorage.db.sub_value(emoji, messageID, authorID, channelID);
+
+            await cbStorage.updateLeaderboard();
+        }
     },
 };
