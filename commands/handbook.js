6caef0f77d85ff2c9fd719dbd01167bd6b3a554b
Jack Li <xenocidepc@gmail.com>
2021-10-04 23:36:37 +1100

    Implement handbook

--- commands/handbook.js
+++ commands/handbook.js
@@ -1,3 +1,5 @@
+const axios = require("axios");
+const textVersion = require("textversionjs");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 const { apiURL, handbookURL } = require("../config/handbook.json");
@@ -16,10 +18,73 @@ module.exports = {
         if (interaction.options.getSubcommand() === "courseinfo") {
             const courseCode = await interaction.options.getString("coursecode").toUpperCase();
 
-            // TODO: Actually get the data lmao
+            let data;
+            try {
+                const response = await axios.get(`${apiURL}${courseCode}`);
+                data = response.data;
+            } catch (e) {
+                return await interaction.reply({ content: "Invalid course code.", ephemeral: true });
+            }
+
+            console.log(data);
+
+            console.log(data["enrolment_requirements"]);
+
             const courseInfo =
                 new MessageEmbed()
-                    .setTitle("Test");
+                    .setTitle(data["title"])
+                    .setURL(`${handbookURL}${courseCode}`)
+                    .setColor(0x3A76F8)
+                    .setAuthor(`Course Info: ${courseCode} (${data["credit_points"]} UOC)`, "https://i.imgur.com/EE3Q40V.png")
+                    .addFields(
+                        // Need to parse (and fix hardcoded length)
+                        {
+                            name: "Overview",
+                            value: textVersion(data["description"])
+                                .substring(0, Math.min(textVersion(data["description"]).indexOf("\n"), 1024)),
+                            inline: false,
+                        },
+                        {
+                            name: "Enrolment Requirements",
+                            value: data["enrolment_requirements"]
+                                .replace(/[A-Z]{4}[0-9]{4}/g, `[$&](${handbookURL}$&)`)
+                                || "None",
+                            inline: true,
+                        },
+                        {
+                            name: "Offering Terms",
+                            value: data["offering_terms"],
+                            inline: true,
+                        },
+                        {
+                            name: "Delivery Mode",
+                            value: data["delivery_mode"],
+                            inline: true,
+                        },
+                        {
+                            name: "Equivalent Courses",
+                            value: data["equivalent_courses"]
+                                .map(course => `[${course}](${handbookURL}${course})`)
+                                .join(", ")
+                                || "None",
+                            inline: true,
+                        },
+                        {
+                            name: "Exclusion Courses",
+                            value: data["exclusion_courses"]
+                                .map(course => `[${course}](${handbookURL}${course})`)
+                                .join(", ")
+                                || "None",
+                            inline: true,
+                        },
+                        {
+                            name: "Course Outline",
+                            value: `[${courseCode} Course Outline](${data["course_outline_url"]})`,
+                            inline: true,
+                        },
+                    )
+                    .setTimestamp()
+                    .setFooter("Data fetched from Zac's Handbook API");
 
             await interaction.reply({ embeds: [courseInfo] });
         }
