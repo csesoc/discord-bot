c4f902376ea15b0b34aefdb75e84c023a0aef628
ollibowers <80164276+ollibowers@users.noreply.github.com>
2021-10-06 03:28:14 +1100

    Commands mostly done, just need testing and tidying

--- commands/carrotboard.js
+++ commands/carrotboard.js
@@ -1,8 +1,9 @@
 //@ts-check
 const { SlashCommandBuilder, SlashCommandSubcommandGroupBuilder, SlashCommandSubcommandBuilder } = require("@discordjs/builders");
-const { CommandInteraction, ContextMenuInteraction, Client } = require("discord.js");
+const { CommandInteraction, ContextMenuInteraction, Client, MessageEmbed } = require("discord.js");
 
-const { CarrotboardStorage } = require("../lib/carrotboard");
+const { CarrotboardStorage, extractOneEmoji } = require("../lib/carrotboard");
+const { DiscordScroll } = require("../lib/discordscroll/scroller");
 
 //////////////////////////////////////////////
 ////////// SETTING UP THE COMMANDS ///////////
@@ -70,8 +71,8 @@ async function handleInteraction(interaction) {
     /** @type {CarrotboardStorage} */
     const cbStorage = global.cbStorage;
 
-    const commandGroup = interaction.options.getSubcommandGroup();
-    const subcommand = interaction.options.getSubcommand();
+    const commandGroup = interaction.options.getSubcommandGroup(false);
+    const subcommand = interaction.options.getSubcommand(false);
     if (commandGroup == "admin") {
         switch (subcommand) {
             case "carrot":
@@ -108,8 +109,25 @@ async function handleInteraction(interaction) {
  * @param {CarrotboardStorage} cbStorage
  */
 async function handleCBACarrot(interaction, cbStorage) {
-    await interaction.reply("admin carrot");
-    // do stuff
+    // known something given
+    const messageStr = interaction.options.getString("emoji");
+
+    // emoji was given get the emoji
+    const result = extractOneEmoji(messageStr);
+    if (result == null) {
+        // none were given
+        await interaction.reply({ content: "Please give one emoji.", ephemeral: true });
+        return;
+    } else if (result.index != 0) {
+        // not at the front
+        await interaction.reply({ content: "Please give only an emoji.", ephemeral: true });
+        return;
+    }
+
+    // successful
+    cbStorage.config.carrot = result.emoji;
+    cbStorage.config.saveToFile();
+    await interaction.reply({ content: `Carrot emoji set to ${result.emoji}`, ephemeral: true });
 }
 
 /** 
@@ -117,9 +135,6 @@ async function handleCBACarrot(interaction, cbStorage) {
  * @param {CarrotboardStorage} cbStorage
  */
 async function handleCBAOutput(interaction, cbStorage) {
-    // console.log("Setting Channel");
-    // console.log(interaction);
-
     // get details
     const channelID = interaction.channelId;
     const choice = interaction.options.getString("type");
@@ -130,6 +145,7 @@ async function handleCBAOutput(interaction, cbStorage) {
         try {
             cbStorage.config.alertChannelID = channelID;
             cbStorage.config.saveToFile();
+
             await interaction.reply({content: "Alert Output Channel Set.", ephemeral: true});
         } catch (e) {
             console.error(e);
@@ -143,6 +159,7 @@ async function handleCBAOutput(interaction, cbStorage) {
             const message = await interaction.channel.send({embeds: [leaderboard[0]]});
             cbStorage.config.leaderboardID = message.id;
             cbStorage.config.saveToFile();
+
             await interaction.reply({content: "Leaderboard Channel Set.", ephemeral: true});
         } catch (e) {
             console.error(e);
@@ -156,8 +173,11 @@ async function handleCBAOutput(interaction, cbStorage) {
  * @param {CarrotboardStorage} cbStorage
  */
 async function handleCBMain(interaction, cbStorage) {
-    await interaction.reply("main");
-    // do stuff
+    // generate leaderboard and send it
+    const pages = await cbStorage.generateLeaderboard({emoji: String(cbStorage.config.carrot)});
+    
+    const scroller = new DiscordScroll(pages);
+    await scroller.send(interaction);
 }
 
 /** 
@@ -165,8 +185,17 @@ async function handleCBMain(interaction, cbStorage) {
  * @param {CarrotboardStorage} cbStorage
  */
 async function handleCBUser(interaction, cbStorage) {
-    await interaction.reply("user");
-    // do stuff
+    // get the user in question
+    let user = interaction.options.getUser("username");
+    if (user == null) {
+        user = interaction.user;
+    }
+    const userID = user.id;
+
+    // get data
+    const pages = await cbStorage.generateLeaderboard({userID: String(userID)});
+    const scroller = new DiscordScroll(pages);
+    await scroller.send(interaction);
 }
 
 /** 
@@ -174,8 +203,32 @@ async function handleCBUser(interaction, cbStorage) {
  * @param {CarrotboardStorage} cbStorage
  */
 async function handleCBAll(interaction, cbStorage) {
-    await interaction.reply("emoji");
-    // do stuff
+    // check if emoji given
+    const messageStr = interaction.options.getString("emoji", false);
+    if (messageStr == null) {
+        // no emoji given
+        const pages = await cbStorage.generateLeaderboard({});
+        const scroller = new DiscordScroll(pages);
+        await scroller.send(interaction);
+        return;
+    }
+
+    // emoji was given get the emoji
+    const result = extractOneEmoji(messageStr);
+    if (result == null) {
+        // none were given
+        await interaction.reply({ content: "Please give one emoji.", ephemeral: true });
+        return;
+    } else if (result.index != 0) {
+        // not at the front
+        await interaction.reply({ content: "Please give only an emoji.", ephemeral: true });
+        return;
+    }
+
+    // all good
+    const pages = await cbStorage.generateLeaderboard({emoji: result.emoji});
+    const scroller = new DiscordScroll(pages);
+    await scroller.send(interaction);
 }
 
 /** 
@@ -183,8 +236,36 @@ async function handleCBAll(interaction, cbStorage) {
  * @param {CarrotboardStorage} cbStorage
  */
 async function handleCBID(interaction, cbStorage) {
-    await interaction.reply("id");
-    // do stuff
+    // is required so known not null
+    const id = interaction.options.getInteger("cbid", true);
+    
+    // get the entry
+    const entry = await cbStorage.db.get_by_cb_id(id);
+    if (entry == null) {
+        await interaction.reply({ content: "Please include a valid carrotboard ID!", ephemeral: true });
+        return;
+    }
+
+    // get the details
+    const guildID = cbStorage.config.guildID;
+    const channelID = entry["channel_id"];
+    const messageID = entry["message_id"];
+    const url = `https://discord.com/channels/${guildID}/${channelID}/${messageID}`;
+    let content = entry["message_contents"].trimEnd();
+    if (content.length >= this.maxMsgLen) {
+        content = content.slice(0, this.maxMsgLen) + "...";
+    } else if (content.length) {
+        
+    }
+
+    // create the embed
+    const embed = new MessageEmbed({
+        title: `Carrotboard ${id}`,
+        description: content,
+        color: Math.floor(Math.random() * 16581374),
+    });
+
+    await interaction.reply({ embeds: [embed] });
 }
 
 
--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,25 @@
+//@ts-check
+
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const fetch = require("node-fetch");
+
+async function handle(interaction) {
+    const endpoint = "https://handbook.insou.dev/api/v1/course/";
+    const course = "comp1511";
+
+    const res = await fetch(endpoint + course);
+    const data = await res.json();
+
+    console.log(data);
+    interaction.reply(JSON.stringify(data));
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with Pong!"),
+
     async execute(interaction) {
-        await interaction.reply("🏓 Pong!");
+        await handle(interaction);
     },
 };
