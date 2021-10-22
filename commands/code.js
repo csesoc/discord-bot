06ed91ea9cc4e776c4a95fd9ae611c2fa06a6c7b
Jack Li <xenocidepc@gmail.com>
2021-10-22 17:24:05 +1100

    Implement one time codes, expiry and invalidation

--- commands/code.js
+++ commands/code.js
@@ -1,7 +1,8 @@
+const fs = require("fs");
 const voucher_codes = require("voucher-code-generator");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { Permissions } = require("discord.js");
-const { activeCodes } = require("../config/code.json");
+const { redeemableCodes } = require("../config/code.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -17,18 +18,44 @@ module.exports = {
                 .setName("generate")
                 .setDescription("[ADMIN] Generates a redeemable code.")
                 .addStringOption(option => option.setName("prefix").setDescription("Prefix of generated codes").setRequired(true))
-                .addIntegerOption(option => option.setName("count").setDescription("Number of codes to generate")))
+                .addIntegerOption(option => option.setName("count").setDescription("Number of codes to generate"))
+                .addStringOption(option => option.setName("expiry").setDescription("Expiry date and time of codes (JS supported format)"))
+                .addBooleanOption(option => option.setName("onetime").setDescription("Whether the codes should only be usable once")))
         .addSubcommand(subcommand =>
             subcommand
-                .setName("activelist")
-                .setDescription("[ADMIN] Displays the list of active codes.")),
+                .setName("invalidate")
+                .setDescription("[ADMIN] Invalidates an active code.")
+                .addStringOption(option => option.setName("code").setDescription("Code to invalidate").setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("list")
+                .setDescription("[ADMIN] Displays the list of codes.")),
     async execute(interaction) {
         if (interaction.options.getSubcommand() === "redeem") {
             const code = interaction.options.getString("code");
 
-            if (!activeCodes.some(c => c === code)) {
+            const result = redeemableCodes.find(el => el.code === code);
+
+            if (!result || !result.valid) {
                 return await interaction.reply({ content: "Invalid code!", ephemeral: true });
             }
+
+            if (result.users.includes(interaction.member.id)) {
+                return await interaction.reply({ content: "You have already redeemed this code!", ephemeral: true });
+            }
+
+            if (result.oneTime && result.users.length > 0) {
+                return await interaction.reply({ content: "One time code has already been redeemed!", ephemeral: true });
+            }
+
+            if (result.expiry !== -1 && result.expiry < Date.now()) {
+                return await interaction.reply({ content: "Code has expired!", ephemeral: true });
+            }
+
+            result.users.push(interaction.member.id);
+
+            // The path here is different to the require because it's called from index.js (I think)
+            fs.writeFileSync("./config/code.json", JSON.stringify({ redeemableCodes }, null, 4));
 
             await interaction.reply({ content: "Code redeemed successfully!", ephemeral: true });
         }
@@ -39,20 +66,56 @@ module.exports = {
         }
 
         if (interaction.options.getSubcommand() === "generate") {
+            const prefix = await interaction.options.getString("prefix");
+            const count = await interaction.options.getInteger("count");
+            const expiry = Date.parse(await interaction.options.getString("expiry")) || -1;
+            const oneTime = await interaction.options.getBoolean("onetime") ?? false;
+
             const codes = voucher_codes.generate({
-                prefix: await interaction.options.getString("prefix"),
-                count: await interaction.options.getInteger("count"),
-                pattern: "-##########",
-            });
-            activeCodes.push(...codes);
+                prefix: prefix,
+                count: count,
+                pattern: "-####################",
+            }).map(code => ({ code, expiry, oneTime, valid: true, users: [] }));
 
-            // TODO: Save activeCodes to json
-            console.log(activeCodes);
+            console.log(codes);
 
-            await interaction.reply(`Generated codes:\n\`\`\`${codes.join("\n")}\`\`\``);
-        } else if (interaction.options.getSubcommand() === "activelist") {
+            redeemableCodes.push(...codes);
+
+            // The path here is different to the require because it's called from index.js (I think)
+            fs.writeFileSync("./config/code.json", JSON.stringify({ redeemableCodes }, null, 4));
+
+            await interaction.reply(
+                "Generated codes:\n" +
+                "```\n" +
+                `${codes.map(el => el.code).join("\n")}\n` +
+                "```\n" +
+                `Expiry: ${expiry === -1 ? "Never" : (new Date(expiry)).toString()}\n` +
+                `One time: ${oneTime ? "Yes" : "No"}\n`,
+            );
+        } else if (interaction.options.getSubcommand() === "invalidate") {
+            const code = interaction.options.getString("code");
+
+            const result = redeemableCodes.find(el => el.code === code);
+
+            if (!result || !result.valid) {
+                return await interaction.reply({ content: "Code is already invalid!", ephemeral: true });
+            }
+
+            result.valid = false;
+
+            // The path here is different to the require because it's called from index.js (I think)
+            fs.writeFileSync("./config/code.json", JSON.stringify({ redeemableCodes }, null, 4));
+
+            await interaction.reply(`Invalidated code: \`${code}\``);
+        } else if (interaction.options.getSubcommand() === "list") {
             // Really temporary display
-            await interaction.reply(`Active codes:\n\`\`\`${activeCodes.join("\n")}\`\`\``)
+            // TODO: Embed/paginated display with information about expiry, users, etc.
+            await interaction.reply(
+                "Codes:\n" +
+                "```\n" +
+                `${redeemableCodes.map(el => el.code).join("\n")}\n` +
+                "```",
+            );
         }
     },
 };
