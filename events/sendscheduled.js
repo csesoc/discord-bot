67629c981fa2814a9850ccecd60d830dd807fc66
Emily Lu <emily.lu@outlook.com>
2022-03-24 14:55:15 +1100

    remove already sent messages

--- events/sendscheduled.js
+++ events/sendscheduled.js
@@ -6,6 +6,7 @@ module.exports = {
     once: true,
     execute(client) {
         let timer = setInterval(function() {
+            sent_msgs = []
             fs.readFile(path.join(__dirname, '../data/schedulemessage.json'), (err, jsonString) => {
                 if (err) {
                     console.log("Error reading file from disk:", err)
@@ -18,14 +19,27 @@ module.exports = {
                         var today = new Date();
                         now_time = new Date(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours(), today.getMinutes(), 0);
                         if (now_time - send_time == 0) {
+                            sent_msgs.push(index);
                             message = item[0];
                             channel_id = item[1];
                             send_channel = client.channels.cache.get(channel_id);
                             send_channel.send(message);
                         };
                     });
-
+                    
+                    if (sent_msgs.length) {
+                        for (var i = sent_msgs.length - 1; i >= 0; i--) {
+                            to_send.splice(sent_msgs[i], 1)
+                        }
+                        jsonData = JSON.stringify(to_send);
+                        fs.writeFile(path.join(__dirname, '../data/schedulemessage.json'), jsonData, function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        })
+                    }
                 }
+                 
             });
             
         }, 1000 * 60)
