aaed99c70d0fa5b249164e26723b8e481a1c489c
sanyamjain47 <sanyamjain47@gmail.com>
2022-04-14 17:14:57 +1000

    Added logging on console

--- index.js
+++ index.js
@@ -45,4 +45,8 @@ client.on("interactionCreate", async interaction => {
     }
 });
 
+client.on('shardError', error => {
+	console.error('A websocket connection encountered an error:', error);
+});
+
 client.login(process.env.DISCORD_TOKEN);
