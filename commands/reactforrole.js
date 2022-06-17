84e485b38bf03efb069cdad4179d360dcf004d51
Emily Lu <emily.lu@outlook.com>
2022-06-17 23:17:35 +1000

    finished most of the implementation except remove role when unreact

--- commands/reactforrole.js
+++ commands/reactforrole.js
@@ -6,40 +6,24 @@ const path = require("path");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("reactforrole")
-        .setDescription("Users can react for a temporary role")
-        .addSubcommand(subcommand => 
-            subcommand
-                .setName("create")
-                .setDescription("Creates a new text channel and assigns role to anyone who reacts with given emoji")
-                .addStringOption(option => 
-                    option
-                    .setName('emoji')
-                    .setDescription("Enter the emoji users will use to gain the new role")
-                    .setRequired(true)
-                )
-                .addStringOption(option => 
-                    option
-                    .setName('rolename')
-                    .setDescription("Enter the name of the role")
-                    .setRequired(true)
-                )
-                .addStringOption(option => 
-                    option
-                    .setName('message')
-                    .setDescription("Enter your message")
-                    .setRequired(true)
-                )
+        .setDescription("Creates a new role and assigns role to anyone who reacts with given emoji")
+        .addStringOption(option => 
+            option
+            .setName('emojis')
+            .setDescription("Enter one or more emojis users will use to gain the new role separated by commas (e.g. emoji,emoji)")
+            .setRequired(true)
         )
-        .addSubcommand(subcommand => 
-            subcommand
-                .setName("remove")
-                .setDescription("Removes role created using the reactforrole create command")
-                .addStringOption(option => 
-                    option
-                    .setName('rolename')
-                    .setDescription("Enter the name of the role to be removed")
-                    .setRequired(true)
-                )
+        .addStringOption(option => 
+            option
+            .setName('rolenames')
+            .setDescription("Enter the names of the roles separated by commas (e.g. rolename,rolename)")
+            .setRequired(true)
+        )
+        .addStringOption(option => 
+            option
+            .setName('message')
+            .setDescription("Enter your message")
+            .setRequired(true)
         ),
 
     async execute(interaction) {
@@ -51,80 +35,95 @@ module.exports = {
             });
         }
 
-        const command = interaction.options.getSubcommand();
+        const emojis = interaction.options.getString('emojis');
+        const roleNames = interaction.options.getString('rolenames');
+        const messsage = interaction.options.getString('message');
+
+        const emojiList = emojis.split(",").map(item => item.trim());
+        const roleList = roleNames.split(",").map(item => item.trim());
         
-        if (command === "create") {
+        // Check emojis are unique
+        if (emojiList.length !== new Set(emojiList).size) {
+            return await interaction.reply({
+                content: "Please enter unique emojis",
+                ephemeral: true
+            });
+        }
 
-            const emoji = interaction.options.getString('emoji');
-            const roleName = interaction.options.getString('rolename');
-            const messsage = interaction.options.getString('message');
-
-            // Check if emoji input is an emoji
-            var emoji_regex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])$/;
-            if (!emoji_regex.test(emoji)) {
+        // Check all emojis are valid
+        const unicode_emoji_regex = /^(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])$/;
+        const custom_emoji_regex = /^<:.*:\d{18}>$/
+        for (element of emojiList) {
+            if (!unicode_emoji_regex.test(element) && !custom_emoji_regex.test(element)) {
                 return await interaction.reply({
-                    content: "Please enter one unicode emoji character only.",
+                    content: "Please enter emojis only separated by commas e.g. emoji,emoji",
                     ephemeral: true
                 });
             }
-
-            fs.readFile(path.join(__dirname, '../data/tmpreactroles.json'), async (err, jsonString) => {
-                if (err) {
-                    console.log("Error reading file from disk:", err);
-                    return;
-                }
-                else {
-                    let data = JSON.parse(jsonString);
-
-                     // Check if rolename already exists
-                    if (roleName in data) {
-                        return await interaction.reply({
-                            content: "This role already exists",
-                            ephemeral: true
-                        });
-                    }
-
-                    // Send message
-                    const sent_message = await interaction.reply({
-                        content: messsage,
-                        fetchReply: true
-                    });
-
-                    // Add react
-                    sent_message.react(emoji)
-
-                    // Add to data
-                    data[roleName] = {
-                        messageID: sent_message.id,
-                        emoji: emoji
-                    };
-
-                    jsonData = JSON.stringify(data);
-                    fs.writeFile(path.join(__dirname, '../data/tmpreactroles.json'), jsonData, function(err) {
-                        if (err) {
-                            console.log(err);
-                        }
-                    })
-                }
-            });
-
-            // Create a new role with data and a reason
-            interaction.member.guild.roles.create({
-                name: roleName,
-                reason: `we needed this cool new role "${roleName}"`,
-            }).catch(console.error);
-
-            // Create channel
-            let name = interaction.user.username;
-            interaction.member.guild.channels.create(roleName)
-                .catch(console.error);
-
-        } else if (command === "remove") {
-
-            const roleName = interaction.option.getString('rolename');
-
         }
 
+        // Check if rolenames and emojis correspond 
+        if (emojiList.length != roleList.length) {
+            return await interaction.reply({
+                content: "Please have a rolename correspond to each emoji, ensure emojis and role names are separated by commas",
+                ephemeral: true
+            });
+        }
 
+        fs.readFile(path.join(__dirname, '../data/tmpreactroles.json'), async (err, jsonString) => {
+            if (err) {
+                console.log("Error reading file from disk:", err);
+                return;
+            }
+            let data = JSON.parse(jsonString);
+
+            messageInfo = {
+                senderID: interaction.user.id,
+                roles: {}
+            };
+
+            for (let i = 0; i < roleList.length; i++) {
+                let roleName = roleList[i];
+                let emoji = emojiList[i];
+                
+                // Check if role exist
+                let role = interaction.member.guild.roles.cache.find(role => role.name === roleName);
+                let roleID;
+
+                if (role) {
+                    roleID = role.id;
+                } else {
+                    // Role does not exist so create one
+                    let newRole = await interaction.member.guild.roles.create({
+                        name: roleName,
+                        reason: `we needed this cool new role "${roleName}"`,
+                    }).catch(console.error);
+
+                    roleID = newRole.id;
+                }
+                messageInfo.roles[emoji] = roleID;
+            } 
+            
+            // Send message
+            const sentMessage = await interaction.reply({
+                content: messsage,
+                fetchReply: true
+            });
+
+            // Add react
+            emojiList.forEach(emoji => {
+                sentMessage.react(emoji)
+            });
+            
+            // Add to data
+            data[sentMessage.id] = messageInfo;
+            
+            jsonData = JSON.stringify(data);
+            fs.writeFile(path.join(__dirname, '../data/tmpreactroles.json'), jsonData, function(err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        });
     },
 };
--- events/givereactrole.js
+++ events/givereactrole.js
@@ -1,4 +1,4 @@
-const { Emoji } = require("discord.js");
+const { MessageEmbed } = require('discord.js');
 const fs = require('fs');
 const path = require("path");
 
@@ -6,34 +6,66 @@ module.exports = {
     name: "messageReactionAdd",
     once: false,
     execute(reaction, user) {
-        console.log(reaction)
+        if (user.bot) return;
+        
         fs.readFile(path.join(__dirname, '../data/tmpreactroles.json'), (err, jsonString) => {
             if (err) {
                 console.log("Error reading file from disk:", err);
                 return;
             }
-
             let data = JSON.parse(jsonString);
-            
-            // BIG PROBLEMOS HERE
-            for (const tmpRole in data) {
-                if (reaction.emoji.name == tmpRole.emoji && reaction.message.id === tmpRole.messageID) {
-                    client.guilds.fetch(reaction.guildId).then(guild => {
-                        guild.fetchMember(user) // fetch the user that reacted
-                        .then((member) => 
-                        {
-                            let role = (guild.roles.find(role => role.name === tmpRole));
-                            member.addRole(role)
-                            .then(() => 
-                                {
-                                    console.log(`Added the role to ${member.displayName}`);
-                                }
-                            );
-                        });
-                    })
-                    
-                }
+
+            // Check if message id is in the data
+            if (!(reaction.message.id in data)) return;
+
+            // Check if emoji is in data
+            if (!data[reaction.message.id].roles[reaction.emoji.name]) return;
+
+            // Check if emoji is ⛔ and if the user is the sender
+            if (reaction.emoji.name === '⛔' && user.id) return;
+
+            // Check if message has ⛔ reacted by the sender
+            // If not assign the role to the user
+            let reactions = reaction.message.reactions;
+            let noEntryReact = reactions.resolve('⛔')
+            let role = data[reaction.message.id].roles[reaction.emoji.name];
+            if (noEntryReact) {
+                noEntryReact.users.fetch().then(userList => {
+                    if (userList.has(data[reaction.message.id].senderID)) {
+                        reactions.resolve(reaction).users.remove(user);
+                        
+                        // Notify user that role was not assigned
+                        let notification = new MessageEmbed()
+                            .setColor('#7cd699')
+                            .setTitle('Role could not be assigned')
+                            .setAuthor("CSESoc Projects Bot", 'https://avatars.githubusercontent.com/u/164179?s=200&v=4')
+                            .setDescription(`You can no longer reacted to the message in "${reaction.message.guild.name}" to get a role`)
+                        user.send({
+                            embeds: [notification]
+                        })
+
+                    } else {
+                        giveRole(reaction, user, role)
+                    }
+                })
+            } else {
+                giveRole(reaction, user, role)
             }
         });
     },
 };
+
+async function giveRole(reaction, user, role) {
+    reaction.message.guild.members.cache.get(user.id).roles.add(role);
+    let roleName = await reaction.message.guild.roles.cache.find(r => r.id === role).name
+
+    // Notify user role was successfully added
+    let notification = new MessageEmbed()
+        .setColor('#7cd699')
+        .setTitle('Roles updated!')
+        .setAuthor("CSESoc Projects Bot", 'https://avatars.githubusercontent.com/u/164179?s=200&v=4')
+        .setDescription(`You reacted to a message in "${reaction.message.guild.name}" and was assigned the "${roleName}" role`)
+    user.send({
+        embeds: [notification]
+    })
+}
