2d133a6d9399d07f00e08a19126c657a9e62b35f
Jason Gong <jasongong1@outlook.com>
2022-06-18 22:17:02 +1000

    tile placing, changed listening for reacts to use collector vs event

--- events/connect4React.js
+++
@@ -1,9 +0,0 @@
-const { handleConnect4React } = require("../lib/connect4/connect4Runner");
-
-module.exports = {
-    once: false,
-    name: "messageReactionAdd",
-    execute(reaction, user) {
-        handleConnect4React(reaction, user);
-    }
-}
