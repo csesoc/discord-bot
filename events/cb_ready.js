e7b8f37a90e4a1d448511768af4648067221e1ce
sanyamjain47 <sanyamjain47@gmail.com>
2022-07-07 09:44:12 +1000

    Carrot Board added

---
+++ events/cb_ready.js
@@ -0,0 +1,10 @@
+const { CarrotboardStorage } = require("../lib/carrotboard");
+
+module.exports = {
+    name: "ready",
+    once: true,
+    execute(client) {
+        const cbStorage = new CarrotboardStorage(client);
+        global.cbStorage = cbStorage;
+    },
+};
