19517a3308eca06a75a46a20b051bbab51019ee1
Yuvraj Singh <yuviuv200@gmail.com>
2022-07-15 08:36:26 +1000

    Repeated word bug (still WIP)

--- commands/wordle.js
+++ commands/wordle.js
@@ -5,6 +5,17 @@ const { players } = require("../config/wordle.json");
 const fs = require("fs");
 const Canvas = require("canvas");
 
+// Letter class
+// Because OOP is always a good idea
+class Letter {
+    constructor(letter, pos, color = 1) {
+        this.letter = letter;
+        this.pos = pos;
+        this.color = color;
+    }
+}
+
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("wordle")
@@ -23,14 +34,10 @@ module.exports = {
                 .setRequired(true),
             ),
         ),
-
-
     async execute(interaction) {
 
         const userID = interaction.user.id;
         // check if userID exists in players value
-
-
         let player = undefined;
 
         for (let i = 0; i < players.length; i++) {
@@ -91,21 +98,102 @@ module.exports = {
             const editedPlayers = players;
             fs.writeFileSync("./config/wordle.json", JSON.stringify({ players: editedPlayers }, null, 4));
         };
-        const GetImage = (guessLetter, answerLetter, i) => {
+        // function to returns every unique letter in a string with count of each letter
+        const eachLetterCount = (answer) => {
+            const letters = answer.split("");
+            const letterCount = {};
+            letters.forEach(letter => {
+                if (letterCount[letter]) {
+                    letterCount[letter]++;
+                } else {
+                    letterCount[letter] = 1;
+                }
+            });
+            return letterCount;
+        };
+        const GetImage = (guessLetter, answerLetter, i, answerCount, guessCount, greenCount, yellowCount) => {
+
             // letter is undefined
             if (guessLetter === undefined) {
                 return 0;
             } else if (guessLetter.charAt(i) == answerLetter.charAt(i)) {
+                // letter is correct
+                greenCount[guessLetter.charAt(i)]++;
                 return 2;
             // letter is in word at same spot
             } else if (answerLetter.includes(guessLetter.charAt(i))) {
-                return 3;
+                const total_count = greenCount[guessLetter.charAt(i)] + yellowCount[guessLetter.charAt(i)];
+                if (total_count < answerCount[guessLetter.charAt(i)]) {
+                    yellowCount[guessLetter.charAt(i)]++;
+                    return 3;
+                } else {
+                    return 1;
+                }
             // letter is in word at different spot
             } else {
                 return 1;
             }
             // letter is not in word
         };
+        const getAnswer = (answer, guess) => {
+            console.log("Answer: " + answer);
+            console.log("Guess: " + guess);
+            const answerCount = eachLetterCount(answer);
+            const colors = [];
+            const greenCount = {};
+            const yellowCount = {};
+            for (let i = 0; i < answer.length; i++) {
+                colors[i] = 0;
+                greenCount[answer.charAt(i)] = 0;
+                yellowCount[answer.charAt(i)] = 0;
+            }
+            if (guess === undefined) {
+                // return array of 6 0s
+                return colors;
+            }
+            // Check for exact matches in position
+            for (let i = 0; i < answer.length; i++) {
+                if (guess.charAt(i) == answer.charAt(i)) {
+                    colors[i] = 2;
+                    console.log("\t green count of " + answer.charAt(i) + ": " + greenCount[answer.charAt(i)]);
+                    greenCount[answer.charAt(i)]++;
+                    console.log("\t green count: " + greenCount[answer.charAt(i)]);
+                }
+            }
+            // Check for matches in word
+            for (let i = 0; i < answer.length; i++) {
+                console.log("\t answer letter: " + answer.charAt(i));
+
+
+                const maxCount = answerCount[answer.charAt(i)] - greenCount[answer.charAt(i)];
+                console.log("\t maxCount: " + maxCount);
+                console.log(guess.includes(answer.charAt(i)));
+                if (guess.includes(answer.charAt(i))) {
+                    console.log(greenCount);
+                    console.log(yellowCount);
+                    console.log(answerCount);
+                    console.log("Max Count for " + answer.charAt(i) + ": " + maxCount);
+                    console.log("answer[charAt(i)]: " + answer.charAt(i));
+                    if (maxCount > 0) {
+                        console.log("Adding yellow");
+                        console.log("answerCount[answer.charAt(i)], i: " + answerCount[answer.charAt(i)] + ", " + i);
+                        console.log("greenCount[answer.charAt(i)], i: " + greenCount[answer.charAt(i)] + ", " + i);
+                        yellowCount[answer.charAt(i)]++;
+                        colors[i] = 3;
+                    }
+                }
+            }
+            
+            // make all other letters 1
+            for (let i = 0; i < answer.length; i++) {
+                if (colors[i] < 1) {
+                    colors[i] = 1;
+                }
+            }
+            return colors;
+        };
+
+
         async function LoadGame(msg, guesses, answer) {
             // make a blank canvas
             const canvas = Canvas.createCanvas(330, 397);
@@ -129,17 +217,18 @@ module.exports = {
             let rowOffset = 0;
             let buffer = 0;
 
+
             for (let j = 0; j < 6; j++) {
+                const imageNums = getAnswer(answer, guesses[j]);
                 for (let i = 0; i < 5; i++) {
                     // eslint-disable-next-line no-undef
-                    const imageNumber = GetImage(guesses[j], answer, i);
+                    const imageNumber = imageNums[i];
                     square = square_arr[imageNumber];
 
                     context.drawImage(square, i * squareSize + buffer, rowOffset, squareSize, squareSize);
                     if (guesses[j] != undefined) {
                         context.fillText(guesses[j].charAt(i), (squareSize / 2) + buffer + squareSize * i, rowOffset + 42);
                     }
-
                     buffer += 5;
                 }
                 buffer = 0;
@@ -175,23 +264,28 @@ module.exports = {
             this.guesses = player.guesses;
             // Start a new game
             console.log("Selected word (play): " + selectedWord);
-
             // console.log(interaction);
             LoadGame(interaction, player.guesses, player.wordOfTheDay);
 
         } else if (interaction.options.getSubcommand() === "guess") {
             // Guess a word
-            console.log("Guess: " + interaction.options.getString("word"));
+            // console.log("Guess: " + interaction.options.getString("word"));
             if (interaction.options.getString("word") === undefined) {
                 interaction.channel.send("Invalid Usage!\nUsage: `/wordle guess <word>`");
                 return;
             } else {
+                if (this.guesses === undefined) {
+                    this.guesses = player.guesses;
+                }
+
                 const guess = interaction.options.getString("word").toLowerCase();
                 if (guess.length !== 5) {
                     interaction.channel.send("Invalid Usage!\nUsage: `/wordle guess <word>`");
                     return;
                 } else {
                     if (!player.if_finished) {
+                        console.log("Guess: " + guess);
+                        console.log("guesses: " + this.guesses);
                         this.guesses.push(guess);
                         // update guesses in players
                         player.guesses = this.guesses;
