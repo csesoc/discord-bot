927e4c347630dfd156ad45907f4b950d8f7bebd9
Riz Manter <riz.manter11@gmail.com>
2022-07-27 15:15:04 +1000

    Anonymous message different channel feature

--- events/log_ready.js
+++ events/log_ready.js
@@ -11,13 +11,16 @@ module.exports = {
         (async() => {
             await logDB.create_tables();
             for(let i = 0; i < guilds.length; i++) {
+
                 let g = client.guilds.cache.get(guilds[i])
                 let channels = g.channels.cache
+
                 channels_arr = [...channels.values()]
                 channels_filtered = channels_arr.filter((c) => (c.type === "GUILD_TEXT"))
+
                 for(let m in channels_filtered){
                     //console.log(channels_filtered[m].id, channels_filtered[m].name);
-                    logDB.channel_add(channels_filtered[m].id, channels_filtered[m].name)
+                    logDB.channel_add(channels_filtered[m].id, channels_filtered[m].name, g.id)
                 }
             }
         })();
