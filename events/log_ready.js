1c1d82529e58fd0944727b4b0d13d047d38fc313
Riz Manter <riz.manter11@gmail.com>
2022-08-01 16:51:10 +1000

    Auto course whitelist

--- events/log_ready.js
+++ events/log_ready.js
@@ -1,4 +1,6 @@
 const { DBlog } = require("../lib/database/dblog");
+const { allowedChannels } = require("../config/anon_channel.json");
+const fs = require('fs');
 
 module.exports = {
     name: "ready",
@@ -7,6 +9,13 @@ module.exports = {
         const guilds = client.guilds.cache.map(guild => guild.id);
         const logDB = new DBlog();
         global.logDB = logDB;
+
+        const courses = ["comp", "seng", "binf", "engg", "math"]
+
+
+        function checkCourse(c_name, courses) {
+            return courses.some(c => c_name.startsWith(c.toLowerCase()));
+        }
 
         (async() => {
             await logDB.create_tables();
@@ -20,7 +29,17 @@ module.exports = {
 
                 for(let m in channels_filtered){
                     //console.log(channels_filtered[m].id, channels_filtered[m].name);
-                    logDB.channel_add(channels_filtered[m].id, channels_filtered[m].name, g.id)
+                    c_name = channels_filtered[m].name;
+                    c_id = channels_filtered[m].id;
+
+                    if(checkCourse(c_name, courses)){
+                        if (!allowedChannels.some(c => c === c_id)) {
+                            allowedChannels.push(c_id);
+                            fs.writeFileSync("./config/anon_channel.json", JSON.stringify({ allowedChannels: allowedChannels }, null, 4));
+                        }
+                    }
+
+                    logDB.channel_add(c_id, c_name, g.id)
                 }
             }
         })();
