69411f9d941b4263576bfec50fc5e1c608ecc7d0
Emily Lu <emily.lu@outlook.com>
2022-09-07 19:00:21 +1000

    style edits

--- commands/schedulepost.js
+++ commands/schedulepost.js
@@ -57,24 +57,15 @@ module.exports = {
             });
         }
 
-        var command = interaction.options.getSubcommand();
+        const command = interaction.options.getSubcommand();
+        const msg_id = interaction.options.getString('messageid');
+        const channel = interaction.options.getChannel('channel');
+        const datetime = interaction.options.getString('datetime');
 
         if (command === "create") {
-            create_scheduled_post(interaction)
+            create_scheduled_post(interaction, msg_id, channel, datetime)
         } else if (command === "cancel") {
-            const msg_id = interaction.options.getString('messageid');
-            const send_channel_obj = interaction.options.getChannel('channel');
-            const channel_name = send_channel_obj.name;
-            const send_channel_id = send_channel_obj.id;
-            const datetime = interaction.options.getString('datetime');
-            const schedule_post_id = await schedulePost.get_scheduled_post_id(msg_id, send_channel_id, datetime)
-
-            if (schedule_post_id) {
-                await schedulePost.remove_scheduled(schedule_post_id)
-                await interaction.reply({content: "Message #" + msg_id + " scheduled for channel '" + channel_name + " at " + datetime +  "' was successfully deleted!"});
-            } else {
-                await interaction.reply({content: "No matching scheduled message was found."})
-            }
+            cancel_scheduled_post(interaction, msg_id, channel, datetime)
         }
     }
 };
@@ -82,40 +73,43 @@ module.exports = {
 
 
 // Schedules a new post
-async function create_scheduled_post(interaction) {
-    const message_id = interaction.options.getString('messageid');
-
+async function create_scheduled_post(interaction, msg_id, channel, datetime) {
+    
     // Check if message id is valid
     try {
-        var message = await interaction.channel.messages.fetch(message_id);
+        var message = await interaction.channel.messages.fetch(msg_id);
     } catch (err) {
-        console.log(err)
-        return await interaction.reply({ content: "Invalid message ID", ephemeral: true });
+        return await interaction.reply({
+            content: "Invalid message ID", 
+            ephemeral: true 
+        });
     }
 
-    const send_channel_obj = interaction.options.getChannel('channel');
-    const channel_name = send_channel_obj.name;
-
     let re = /^\d{4}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01]) ([01]\d|2[0-3]):([0-5]\d)$/;
-    const datetime = interaction.options.getString('datetime');
     
     // Check if datetime is valid
     if (!re.test(datetime)) {
-        await interaction.reply( { content: "Please enter the datetime as YYYY-MM-DD HH:MM exactly", ephemeral: true});
-        return;
+        return await interaction.reply({
+            content: "Please enter the datetime as YYYY-MM-DD HH:MM exactly",
+            ephemeral: true
+        });
     }
 
     // If reminders are needed, check if the date and time of the reminder is valid
     const user_reminder = interaction.options.getString('reminder')
     if (user_reminder && !re.test(user_reminder)) {
-        await interaction.reply( { content: "Please enter the reminder as YYYY-MM-DD HH:MM exactly", ephemeral: true});
-        return;
+        return await interaction.reply({ 
+            content: "Please enter the reminder as YYYY-MM-DD HH:MM exactly", 
+            ephemeral: true
+        });
     }
 
     // Check that the reminder date is after the datetime of when the message is scheduled for
     if (user_reminder && user_reminder <= datetime) {
-        await interaction.reply( { content: "Please enter a reminder date that is after the datetime of the scheduled post", ephemeral: true});
-        return;
+        return await interaction.reply({
+            content: "Please enter a reminder date that is after the datetime of the scheduled post", 
+            ephemeral: true
+        });
     }
 
     var send_time = new Date(datetime);
@@ -125,8 +119,10 @@ async function create_scheduled_post(interaction) {
 
     // Check that the datetime the post is scheduled for is in the futre
     if (time_send_in <= 0) {
-        await interaction.reply( { content: "Please enter a datetime in the future for 'datetime'", ephemeral: true});
-        return;
+        return await interaction.reply({
+            content: "Please enter a datetime in the future for 'datetime'", 
+            ephemeral: true
+        });
     }
 
     // Calculate how long until the scheduled message will be sent
@@ -146,23 +142,23 @@ async function create_scheduled_post(interaction) {
     
     // Create message preview
     const preview = new MessageEmbed()
-    .setColor('#C492B1')
-    .setTitle('Message Preview')
-    .setDescription(message.content.length === 0 ? ' ' : message.content + (user_reminder ? "\n \n react ⏰ to be notified about this event!": ""));
-    
+        .setColor('#C492B1')
+        .setTitle('Message Preview')
+        .setDescription(message.content.length === 0 ? ' ' : message.content + (user_reminder ? "\n \n react ⏰ to be notified about this event!": ""));
+        
     // Add scheduled post to database
-    await schedulePost.add_react_role_msg(interaction.guildId , message_id, interaction.channelId, send_channel_obj.id, datetime, user_reminder)
+    await schedulePost.add_react_role_msg(interaction.guildId , msg_id, interaction.channelId, channel.id, datetime, user_reminder)
 
     // Create user reply with all the details of the scheduled post
-    var reply_msg = `Message # ${message_id} for channel '${channel_name}' schedule at ${datetime} which is in ${send_in}.`;
+    var reply_msg = `Message #${msg_id} is scheduled for channel '${channel.name}' at ${datetime} (${send_in}).`;
     var num_msg_attachments = message.attachments.size;
     
     if (num_msg_attachments) {
-        reply_msg += ` This message has ${num_msg_attachments} attachment${num_msg_attachments === 1 ? "": "s"}.`;
+        reply_msg += `\nThis message has ${num_msg_attachments} attachment${num_msg_attachments === 1 ? "": "s"}.`;
     }
 
     if (user_reminder) {
-        reply_msg += ` Reminders will be sent to users who react '⏰' at ${user_reminder}.`;
+        reply_msg += `\nReminders will be sent to users who react '⏰' at ${user_reminder}.`;
     }
 
     await interaction.reply({ 
@@ -170,4 +166,22 @@ async function create_scheduled_post(interaction) {
         ephemeral: false, 
         embeds: [preview]
     });
+}
+
+// Cancels a scheduled post
+async function cancel_scheduled_post(interaction, msg_id, channel, datetime) {
+    const channel_name = channel.name;
+    const channel_id = channel.id;
+    const schedule_post_id = await schedulePost.get_scheduled_post_id(msg_id, channel_id, datetime)
+
+    if (schedule_post_id) {
+        await schedulePost.remove_scheduled(schedule_post_id)
+        await interaction.reply({
+            content: "Message #" + msg_id + " scheduled for channel '" + channel_name + "' at " + datetime +  " was successfully deleted!"
+        });
+    } else {
+        await interaction.reply({
+            content: "No matching scheduled message was found."
+        })
+    }
 }
