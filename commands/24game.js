39082da4f61c0364aeaf112a7d52e3930824de54
Wolfdragon24 <dragonbomber24@gmail.com>
2022-10-06 21:33:13 +1100

    Appended 24 game code to the full bot version of code

--- commands/24game.js
+++ commands/24game.js
@@ -5,13 +5,13 @@ const MAX = 9;
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("24")
-        .setDescription("Generates 4 random numbers from 1 to 10!"),
+        .setDescription("Generates 4 random numbers from 0 to 9!"),
     async execute(interaction) {
         let resultNums = [];
 
         for (let i = 0; i < 4; i++) {
             let random = Math.round(Math.random() * (MAX));
-            resultNums.append(random);
+            resultNums.push(random);
         }
 
         let output = `Your numbers are: ${resultNums.join(" ")}`;
