84620e0f89ad554c07e4993b0304c65bd9dce32d
Wolfdragon24 <31204305+Wolfdragon24@users.noreply.github.com>
2022-10-07 09:47:46 +1100

    Appending 24 game feature (#84)
    * Appending initial file for 24 game command
    
    * Appended file for 24 game, utilising js-port-template as the root branch due to errors with main branch
    
    * Appended 24 game code to the full bot version of code

---
+++ commands/24game.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const MAX = 9;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("24")
+        .setDescription("Generates 4 random numbers from 0 to 9!"),
+    async execute(interaction) {
+        let resultNums = [];
+
+        for (let i = 0; i < 4; i++) {
+            let random = Math.round(Math.random() * (MAX));
+            resultNums.push(random);
+        }
+
+        let output = `Your numbers are: ${resultNums.join(" ")}`;
+
+        await interaction.reply(output);
+    },
+};
