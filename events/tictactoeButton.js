2b4a662431e818fda4b998170675f95b71d52047
Riz Manter <riz.manter11@gmail.com>
2022-10-17 16:55:21 +1100

    tictactoe button fix

--- events/tictactoeButton.js
+++ events/tictactoeButton.js
@@ -4,7 +4,11 @@ module.exports = {
     once: false,
     name: "interactionCreate",
     execute(interaction) {
-        if (!interaction.isButton()) return;
-        handleGameButton(interaction);
+        if (interaction.isButton() && interaction.message.interaction.commandName == "tictactoe"){
+            handleGameButton(interaction);
+        }
+        else{
+            return;
+        }
     }
 }
