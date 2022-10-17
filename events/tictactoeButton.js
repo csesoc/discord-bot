08bc0b79e4b329ea6aaa7487f5386697e47aa108
Riz Manter <riz.manter11@gmail.com>
2022-10-17 17:22:01 +1100

    Format fix?

--- events/tictactoeButton.js
+++ events/tictactoeButton.js
@@ -4,10 +4,9 @@ module.exports = {
     once: false,
     name: "interactionCreate",
     execute(interaction) {
-        if (interaction.isButton() && interaction.message.interaction.commandName == "tictactoe"){
+        if (interaction.isButton() && interaction.message.interaction.commandName == "tictactoe") {
             handleGameButton(interaction);
-        }
-        else{
+        } else {
             return;
         }
     },
