19cbcf180f5964c4555418a4c037eee420210992
Riz Manter <riz.manter11@gmail.com>
2022-10-18 09:46:21 +1100

    Further var const fix

--- commands/logreport.js
+++ commands/logreport.js
@@ -52,9 +52,8 @@ module.exports = {
                 return;
             }
 
-            // eslint-disable-next-line
             const email_reg =
-                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; /* eslint-disable-line */
             const email = interaction.options.getString("email");
 
             if (!email_reg.test(email)) {
