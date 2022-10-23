a0e20151ef428c009e51caf26bf9f939b9124b54
Riz Manter <riz.manter11@gmail.com>
2022-10-23 16:25:59 +1100

    24game fix

--- commands/24game.js
+++ commands/24game.js
@@ -7,14 +7,14 @@ module.exports = {
         .setName("24")
         .setDescription("Generates 4 random numbers from 0 to 9!"),
     async execute(interaction) {
-        let resultNums = [];
+        const resultNums = [];
 
         for (let i = 0; i < 4; i++) {
-            let random = Math.round(Math.random() * MAX);
+            const random = Math.round(Math.random() * MAX);
             resultNums.push(random);
         }
 
-        let output = `Your numbers are: ${resultNums.join(" ")}`;
+        const output = `Your numbers are: ${resultNums.join(" ")}`;
 
         await interaction.reply(output);
     },
