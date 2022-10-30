016d8fa0fe8363766865bcbe9602a3640034336b
Riz Manter <riz.manter11@gmail.com>
2022-10-31 10:20:29 +1100

    Init standup fix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-
 function messagelog(message) {
     // ignore messages sent from bot
     if (message.author.bot) {
@@ -19,74 +17,21 @@ function messagelog(message) {
 module.exports = {
     name: "messageCreate",
     async execute(message) {
-        // console.log(message);
-        let teamName = "";
-        // console.log(message);
+        const standupDB = global.standupDBGlobal;
+
         messagelog(message);
+
         if (message.content.startsWith("$standup")) {
-            let tempData;
-            try {
-                tempData = fs.readFileSync("./config/standup.json", "utf8");
-            } catch (err) {
-                console.error(err);
-            }
-            let data = JSON.parse(tempData)["data"];
-
-            console.log(data);
+            // Get standup content
             const messages = String(message.content);
-            const messageContent = messages.slice(8);
+            const messageContent = messages.slice(8).trim();
             // console.log(message.channel.parent.name)
-            teamName = message.channel.parent.name;
-            const mentions = message.mentions.users;
-            const mentionsArr = [...mentions.values()];
 
-            // Contains the list of all users mentioned in the message
-            const result = mentionsArr.map((a) => a.id);
+            const teamId = message.channel.parentId;
 
-            const voteauthorid = message.author.id;
-            let voteauthorname = message.member.nickname;
-            if (voteauthorname == null) {
-                voteauthorname = message.author.username;
-            }
-            if (data == undefined) {
-                data = {};
-                data[teamName] = [
-                    {
-                        voteauthorid: voteauthorid,
-                        voteauthorname: voteauthorname,
-                        standup: messageContent,
-                        mentions: result,
-                    },
-                ];
-            }
-            if (teamName in data) {
-                let flag = 0;
-                data[teamName].forEach((item) => {
-                    if (item["voteauthorid"] == voteauthorid) {
-                        item["standup"] = messageContent;
-                        item["mentions"] = result;
-                        flag = 1;
-                    }
-                });
-                if (flag == 0) {
-                    data[teamName].push({
-                        voteauthorid: voteauthorid,
-                        voteauthorname: voteauthorname,
-                        standup: messageContent,
-                        mentions: result,
-                    });
-                }
-            } else {
-                data[teamName] = [
-                    {
-                        voteauthorid: voteauthorid,
-                        voteauthorname: voteauthorname,
-                        standup: messageContent,
-                        mentions: result,
-                    },
-                ];
-            }
-            fs.writeFileSync("./config/standup.json", JSON.stringify({ data: data }, null, 4));
+            const standupAuthorId = message.author.id;
+
+            await standupDB.addStandup(teamId, standupAuthorId, message.id, messageContent);
         }
     },
 };
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,6 +1,3 @@
-const { data } = require("../config/standup.json");
-const fs = require("fs");
-
 module.exports = {
     name: "messageUpdate",
     async execute(_oldMessage, message) {
@@ -8,59 +5,32 @@ module.exports = {
         if (message.author.bot == true) {
             return;
         }
+        const standupDB = global.standupDBGlobal;
 
         const logDB = global.logDB;
         logDB.message_update(_oldMessage.id, message.id, message.content);
 
         if (message.content.startsWith("$standup")) {
             const messages = String(message.content);
-            const messageContent = messages.slice(8);
-            // console.log(message.channel.parent.name)
-            const teamName = message.channel.parent.name;
+            const messageContent = messages.slice(8).trim();
 
-            const mentions = message.mentions.users;
-            const mentionsArr = [...mentions.values()];
+            const teamId = message.channel.parentId;
+
+            const standupAuthorId = message.author.id;
+
+            const standupExists = await standupDB.thisStandupExists(message.id);
+            // if this standup exists, update the row else insert new row
+            if (standupExists) {
+                await standupDB.updateStandup(message.id, messageContent);
+            } else {
+                await standupDB.addStandup(teamId, standupAuthorId, message.id, messageContent);
+            }
+
+            // const mentions = message.mentions.users;
+            // const mentionsArr = [...mentions.values()];
 
             // Contains the list of all users mentioned in the message
-            const result = mentionsArr.map((a) => a.id);
-
-            const voteauthorid = message.author.id;
-            let voteauthorname = message.member.nickname;
-            if (voteauthorname == null) {
-                voteauthorname = message.author.username;
-            }
-
-            if (teamName in data) {
-                let flag = 0;
-                data[teamName].forEach(function (item) {
-                    if (item["voteauthorid"] == voteauthorid) {
-                        item["standup"] = messageContent;
-                        item["mentions"] = result;
-                        flag = 1;
-                    }
-                });
-                if (flag == 0) {
-                    data[teamName].push({
-                        voteauthorid: voteauthorid,
-                        voteauthorname: voteauthorname,
-                        standup: messageContent,
-                        mentions: result,
-                    });
-                }
-            } else {
-                data[teamName] = [
-                    {
-                        voteauthorid: voteauthorid,
-                        voteauthorname: voteauthorname,
-                        standup: messageContent,
-                        mentions: result,
-                    },
-                ];
-            }
-            // console.log(data);
-
-            // data.unshift({ 'string': votestring, 'authorid': voteauthorid, 'channelid': channelid, 'messageid': messageid })
-            fs.writeFileSync("./config/standup.json", JSON.stringify({ data: data }, null, 4));
+            // const result = mentionsArr.map((a) => a.id);
         }
     },
 };
