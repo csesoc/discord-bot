cee228722630d7e37367fe20cd466c777e6d5e0c
imagine-hussain <93496985+imagine-hussain@users.noreply.github.com>
2022-10-31 17:03:10 +1100

    24plus (#96)
    * 24plus
    
    * fix boundaries of random
    
    * no zeroes (cringe)
    
    * fix comment for inclusivity of zero and extracxt MAX_TARGET to const

---
+++ commands/24plus.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const MAX = 11;
+const MAX_TARGET = 99;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("24plus")
+        .setDescription("Generates 4 random numbers from 1 to 12 and a random target from 1 to 100."),
+    async execute(interaction) {
+        const resultNums = [];
+
+        for (let i = 0; i < 4; i++) {
+            const random = Math.round(Math.random() * MAX) + 1;
+            resultNums.push(random);
+        }
+
+        const target = Math.round(Math.random() * MAX_TARGET) + 1;
+
+        const output = `Your numbers are: ${resultNums.join(" ")}, with a target of ${target}`;
+
+        await interaction.reply(output);
+    },
+};
