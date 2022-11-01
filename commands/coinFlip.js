0a54b3e894114094abd1cea4c0b304ed3bf031eb
Murtaza Pakawala <mypaka52@gmail.com>
2022-11-02 09:35:28 +1100

    have added embed page to make it look good

--- commands/coinFlip.js
+++ commands/coinFlip.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -6,10 +7,16 @@ module.exports = {
     .setDescription('Tosses a coin 🪙'),
   async execute(interaction) {
     let coinNum = await Math.floor(Math.random() * 2);
+    let coin = coinNum === 0 ? 'heads' : 'tails';
+    let img =
+      coinNum === 0
+        ? 'https://assets.gadgets360cdn.com/img/crypto/dogecoin-og-logo.png'
+        : 'http://assets.stickpng.com/thumbs/5a521f522f93c7a8d5137fc7.png';
+    const embed = new MessageEmbed().setTitle(`its ${coin}!`).setImage(img);
     if (coinNum == 0) {
-      await interaction.reply('Heads');
+      return await interaction.reply({ embeds: [embed] });
     } else {
-      await interaction.reply('Tails');
+      return await interaction.reply({ embeds: [embed] });
     }
   },
 };
