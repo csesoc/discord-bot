03806ccb17937f10f5525040f747bc9c3dffd296
Wolfdragon24 <31204305+Wolfdragon24@users.noreply.github.com>
2022-11-11 12:10:51 +1100

    Implemented parser for 24 game with customisable target (#111)
    * Implemented parser using mathjs library, with default 24 target and optional target input.
    
    * Style fix
    
    * Appended new feature to the help command info data

---
+++ commands/24parser.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const math = require("mathjs");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("24parse")
+        .setDescription("Checks whether an equation evaluates to 24 (or a number input)!")
+        .addStringOption((option) =>
+            option.setName("equation").setDescription("Equation for the 24 game").setRequired(true),
+        )
+        .addNumberOption((option) =>
+            option.setName("target").setDescription("Target for your equation").setRequired(false),
+        ),
+    async execute(interaction) {
+        const equationStr = interaction.options.getString("equation");
+        const equationObj = math.compile(equationStr);
+        const outcome = equationObj.evaluate();
+        const target = interaction.options.getNumber("target") || 24;
+
+        let polarity = "does not equal to";
+        let emoji = "❌";
+
+        if (outcome === target) {
+            polarity = "does equal to";
+            emoji = "✅";
+        }
+
+        const output = `${emoji} The equation: \`\`${equationStr}\`\` evaluates to ${outcome}, which ${polarity} the target ${target}.`;
+
+        await interaction.reply(output);
+    },
+};
