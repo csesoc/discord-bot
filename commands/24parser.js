75b62c2775c77b5ce9ddc2eb791c98e5355c5397
imagine-hussain <hussain.nwz000@gmail.com>
2023-01-11 13:15:08 +1100

    mathjs import fix

--- commands/24parser.js
+++ commands/24parser.js
@@ -50,6 +50,52 @@ const evaluate = (equationString, target) => {
           };
 };
 
+const illegalPhraseRegexes = [ /`/g, /@/g ];
+
+const isIllegalCharactersPresent = (expression) => {
+    return illegalPhraseRegexes.some((regex) => regex.test(expression));
+};
+
+const evaluate = (equationString, target) => {
+    if (isIllegalCharactersPresent(equationString)) {
+        return {
+            success: false,
+            message: "Could not compile. Illegal input detected.",
+            ephemeral: true,
+        };
+    }
+
+    const equationObj = math.compile(equationString);
+    if (!equationObj) {
+        return {
+            success: false,
+            message: "Could not compile. The equation is invalid.",
+            ephemeral: true,
+        };
+    }
+
+    const equationOutcome = equationObj.evaluate();
+    const outcomeAsNumber = Number(equationOutcome);
+    if (isNaN(outcomeAsNumber)) {
+        return {
+            success: false,
+            message: "Could not compile. The equation does not evaluate to a number.",
+            ephemeral: true,
+        };
+    }
+
+    return (outcomeAsNumber == target) ? {
+        success: true,
+        message: `Correct! \`${equationString}\` = ${target}, which is equal to the target`,
+        ephemeral: false,
+    } : {
+        success: false,
+        message: `Incorrect. \`${equationString}\` = ${outcomeAsNumber}, which is not equal to the target`,
+        ephemeral: false,
+    };
+
+};
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("24parse")
@@ -75,3 +121,4 @@ module.exports = {
         });
     },
 };
+
