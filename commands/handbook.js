db3cfdd67cd1269b0eb8441ed99416880d1467b4
imagine-hussain <hussain.nwz000@gmail.com>
2023-01-24 04:28:24 +1100

    migrate from insou api to circles for the handbook commands

--- commands/handbook.js
+++ commands/handbook.js
@@ -27,7 +27,9 @@ module.exports = {
 
             let data;
             try {
-                const response = await axios.get(`${apiURL}${courseCode}`);
+                // Documented at:
+                // https://circlesapi.csesoc.app/docs#/courses/get_course_courses_getCourse__courseCode__get
+                const response = await axios.get(`${apiURL}/courses/getCourse/${courseCode}`);
                 data = response.data;
             } catch (e) {
                 return await interaction.reply({
@@ -36,27 +38,32 @@ module.exports = {
                 });
             }
 
+            const {
+                title, code, uoc, level, description, study_level, school,
+                faculty, equivalents, exclusions, terms, raw_requirements
+            } = data;
+
             const courseInfo = new MessageEmbed()
-                .setTitle(data["title"])
-                .setURL(`${handbookURL}${courseCode}`)
+                .setTitle(title)
+                .setURL(`${handbookURL}/${code}`)
                 .setColor(0x3a76f8)
                 .setAuthor(
-                    `Course Info: ${courseCode} (${data["credit_points"]} UOC)`,
+                    `Course Info: ${code} (${uoc} UOC)`,
                     "https://i.imgur.com/EE3Q40V.png",
                 )
                 .addFields(
                     {
                         name: "Overview",
-                        value: textVersion(data["description"]).substring(
+                        value: textVersion(description).substring(
                             0,
-                            Math.min(textVersion(data["description"]).indexOf("\n"), 1024),
+                            Math.min(textVersion(description).indexOf("\n"), 1024),
                         ),
                         inline: false,
                     },
                     {
                         name: "Enrolment Requirements",
                         value:
-                            data["enrolment_requirements"].replace(
+                            raw_requirements.replace(
                                 /[A-Z]{4}[0-9]{4}/g,
                                 `[$&](${handbookURL}$&)`,
                             ) || "None",
@@ -64,18 +71,13 @@ module.exports = {
                     },
                     {
                         name: "Offering Terms",
-                        value: data["offering_terms"],
-                        inline: true,
-                    },
-                    {
-                        name: "Delivery Mode",
-                        value: data["delivery_mode"],
+                        value: terms.join(", "),
                         inline: true,
                     },
                     {
                         name: "Equivalent Courses",
                         value:
-                            data["equivalent_courses"]
+                        equivalents
                                 .map((course) => `[${course}](${handbookURL}${course})`)
                                 .join(", ") || "None",
                         inline: true,
@@ -83,19 +85,19 @@ module.exports = {
                     {
                         name: "Exclusion Courses",
                         value:
-                            data["exclusion_courses"]
+                            exclusions
                                 .map((course) => `[${course}](${handbookURL}${course})`)
                                 .join(", ") || "None",
                         inline: true,
                     },
-                    {
-                        name: "Course Outline",
-                        value: `[${courseCode} Course Outline](${data["course_outline_url"]})`,
-                        inline: true,
-                    },
+                    /* { */
+                    /*     name: "Course Outline", */
+                    /*     value: `[${courseCode} Course Outline](${data["course_outline_url"]})`, */
+                    /*     inline: true, */
+                    /* }, */
                 )
                 .setTimestamp()
-                .setFooter("Data fetched from Zac's Handbook API");
+                .setFooter("Data fetched from Circles' Api");
 
             await interaction.reply({ embeds: [courseInfo] });
         }
