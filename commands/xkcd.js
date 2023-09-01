a4207eab4ea7d1985467dfd51ab9dbe5c28aebc6
Abraham <ae248abraham@gmail.com>
2023-09-01 17:40:34 +1000

    Refactored and added JSDoc to xkcd.js

---
+++ commands/xkcd.js
@@ -0,0 +1,99 @@
+const {
+    SlashCommandBuilder,
+    InteractionResponse,
+    CommandInteraction,
+    EmbedBuilder
+} = require("discord.js");
+const xkcd = require("xkcd-api");
+
+
+/**
+ * @typedef {Object} xkcdJSON
+ * @property {string} month
+ * @property {string} link
+ * @property {string} year
+ * @property {string} safe_title
+ * @property {string} transcript
+ * @property {string} alt
+ * @property {string} img
+ * @property {string} title
+ * @property {string} day
+ */
+
+/**
+ * @description helper function used to minimise repetitive code 
+ * for xkcd commands
+ * @async
+ * @param {*} err error when requesting an xkcd comic
+ * @param {xkcdJSON} res JSON object containing a successful response w/ comic
+ * @param {CommandInteraction} interaction
+ * @returns {Promise<InteractionResponse<boolean>>}
+ */
+const xkcd_response = async (err, res, interaction) => {
+    if (err) {
+        console.log(err);
+        interaction.reply({
+            content: `sorry something went wrong!😔`,
+            ephemeral: true,
+        });
+    } else {
+        const embed = new EmbedBuilder()
+            .setTitle(res.safe_title)
+            .setImage(res.img);
+        return await interaction.reply({ embeds: [embed] });
+    }
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("xkcd")
+        .setDescription("Replies with a new xkcd joke!")
+        .addSubcommand((subcommand) =>
+            subcommand.setName("latest").setDescription("Get the latest xkcd comic."),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("get")
+                .setDescription("Get xkcd comic by its id.")
+                .addIntegerOption((option) =>
+                    option
+                        .setName("comic-id")
+                        .setRequired(true)
+                        .setDescription("The number id of the xkcd comic you want to get"),
+                ),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand.setName("random").setDescription("Get a random xkcd comic."),
+        ),
+
+    /**
+     *
+     * @async
+     * @param {CommandInteraction} interaction
+     * @returns {Promise<InteractionResponse<boolean>>}
+     */
+
+    async execute(interaction) {
+        switch (interaction.options.getSubcommand()) {
+            case "latest":
+                xkcd.latest(async (err, res) => xkcd_response(err, res, interaction));
+                break;
+            case "get":
+                /**
+                 * @type {Number}
+                 */
+                const comic_id = interaction.options.getInteger("comic-id");
+
+                xkcd.get(
+                    comic_id,
+                    async (err, res) => xkcd_response(err, res, interaction)
+                );
+                break;
+            case "random":
+                xkcd.random(async (err, res) => xkcd_response(err, res, interaction));
+                break;
+            default:
+                break;
+        }
+    },
+};
