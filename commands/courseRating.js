f503622e7f562abe7ab10ddae067e3f9bb15215c
addison <h34312575s@gmail.com>
2024-06-15 22:43:41 +1000

    linting

--- commands/courseRating.js
+++ commands/courseRating.js
@@ -1,34 +1,24 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed, MessageAttachment } = require("discord.js");
-const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
-const puppeteer = require('puppeteer');
+const { ChartJSNodeCanvas } = require("chartjs-node-canvas");
+const puppeteer = require("puppeteer");
 
 async function extractRating(url) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-accelerated-2d-canvas',
-            '--disable-software-rasterizer'
-        ]
-    });
+    const browser = await puppeteer.launch();
 
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    await page.goto(url, { waitUntil: "networkidle2" });
 
-    const courseTitle = await page.$eval('h2.text-3xl.font-bold.break-words', el => el.textContent);
-    const numReviews = await page.$eval('.space-x-2 > span', el => el.textContent);
-    const ratings = await page.$$eval('.flex.flex-wrap.justify-around > div', items => {
+    const courseTitle = await page.$eval(
+        "h2.text-3xl.font-bold.break-words",
+        (el) => el.textContent,
+    );
+    const numReviews = await page.$eval(".space-x-2 > span", (el) => el.textContent);
+    const ratings = await page.$$eval(".flex.flex-wrap.justify-around > div", (items) => {
         const result = [];
-        items.slice(0, 3).forEach(el => {
-            const rating = el.querySelector('.text-2xl.font-bold').textContent;
-            const category = el.querySelector('.text-center.font-bold').textContent;
+        items.slice(0, 3).forEach((el) => {
+            const rating = el.querySelector(".text-2xl.font-bold").textContent;
+            const category = el.querySelector(".text-center.font-bold").textContent;
             result.push({
                 name: category,
                 value: `${rating} out of 5`,
@@ -38,7 +28,7 @@ async function extractRating(url) {
         return result;
     });
 
-    const fullDescription = await page.$eval('.whitespace-pre-line', el => el.textContent);
+    const fullDescription = await page.$eval(".whitespace-pre-line", (el) => el.textContent);
     const description = fullDescription.split(/(?<=[.!?])\s/)[0].trim();
 
     await browser.close();
@@ -47,49 +37,55 @@ async function extractRating(url) {
 
 function ratingColour(rating) {
     if (rating >= 3.5) {
-        return '#39e75f'
+        return "#39e75f";
     } else if (rating > 2.5) {
-        return '#FFA500'
+        return "#FFA500";
     }
-    return '#FF0000'
+    return "#FF0000";
 }
 
 async function buildChart(ratings) {
     const width = 800;
     const height = 300;
-    const averageRating = ratings.reduce((sum, rating) => {
-        return sum + parseFloat(rating.value.split(' ')[0]);
-    }, 0) / ratings.length;
+    const averageRating =
+        ratings.reduce((sum, rating) => {
+            return sum + parseFloat(rating.value.split(" ")[0]);
+        }, 0) / ratings.length;
 
     const canvas = new ChartJSNodeCanvas({ width, height });
 
     const config = {
-        type: 'doughnut',
+        type: "doughnut",
         data: {
-            datasets: [{
-                data: [averageRating, 5 - averageRating],
-                backgroundColor: [ratingColour(averageRating), '#e0e0e0'],
-                borderJoinStyle: 'round',
-                borderRadius: [{
-                    outerStart: 20,
-                    innerStart: 20,
-                }, {
-                    outerEnd: 20,
-                    innerEnd: 20,
-                }],
-                borderWidth: 0,
-            }]
+            datasets: [
+                {
+                    data: [averageRating, 5 - averageRating],
+                    backgroundColor: [ratingColour(averageRating), "#e0e0e0"],
+                    borderJoinStyle: "round",
+                    borderRadius: [
+                        {
+                            outerStart: 20,
+                            innerStart: 20,
+                        },
+                        {
+                            outerEnd: 20,
+                            innerEnd: 20,
+                        },
+                    ],
+                    borderWidth: 0,
+                },
+            ],
         },
         options: {
             rotation: 290,
             circumference: 140,
-            cutout: '88%',
+            cutout: "88%",
             plugins: {
                 legend: {
-                    display: false
+                    display: false,
                 },
-            }
-        }
+            },
+        },
     };
 
     const image = await canvas.renderToBuffer(config);
@@ -100,16 +96,15 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("courserating")
         .setDescription("Tells you the current rating of a specific course!")
-        .addStringOption(option => (
-            option.setName('course'))
-            .setDescription('Enter the course code')
-            .setRequired(true)),
+        .addStringOption((option) =>
+            option.setName("course").setDescription("Enter the course code").setRequired(true),
+        ),
     async execute(interaction) {
-        const course = interaction.options.getString('course');
+        const course = interaction.options.getString("course");
 
         const url = `https://unilectives.devsoc.app/course/${course}`;
 
-        let year = new Date().getFullYear();
+        const year = new Date().getFullYear();
         const handbookUrl = `https://www.handbook.unsw.edu.au/undergraduate/courses/${year}/${course}`;
 
         try {
@@ -118,22 +113,24 @@ module.exports = {
             const { courseTitle, numReviews, description, ratings } = await extractRating(url);
 
             if (numReviews == "0 reviews") {
-                await interaction.editReply({ content: "Sorry there are no reviews for this course yet 😔" });
+                await interaction.editReply({
+                    content: "Sorry there are no reviews for this course yet 😔",
+                });
                 return;
             }
 
             const image = await buildChart(ratings);
-            const attachment = new MessageAttachment(image, 'rating.png');
+            const attachment = new MessageAttachment(image, "rating.png");
             ratings.unshift({
-                name: '\u200B',
+                name: "\u200B",
                 value: `[${course} Handbook](${handbookUrl})`,
             });
             const replyEmbed = new MessageEmbed()
-                .setColor(0x0099FF)
-                .setTitle(course + ' ' + courseTitle)
+                .setColor(0x0099ff)
+                .setTitle(course + " " + courseTitle)
                 .setURL(url)
                 .setDescription(description)
-                .setImage('attachment://rating.png')
+                .setImage("attachment://rating.png")
                 .addFields(...ratings)
                 .setFooter(numReviews);
 
@@ -144,5 +141,5 @@ module.exports = {
                 content: `Sorry the course could not be found! 😔`,
             });
         }
-    }
+    },
 };
